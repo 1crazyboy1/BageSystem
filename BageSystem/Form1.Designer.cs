f20b93a3c572011c46860e93e84bf003930c9ad3
110 <471746662@qq.com>
2022-07-19 15:46:20 +0800

    1

--- BageSystem/Form1.Designer.cs
+++ BageSystem/Form1.Designer.cs
@@ -28,13 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.车道管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pLC设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lED屏幕设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.刷卡器设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.车辆装货确认ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.报表查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.装车日志ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.Bage1Show = new BageSystem.控件.Frame();
+            this.Bage2Show = new BageSystem.控件.Frame();
+            this.Bage3Show = new BageSystem.控件.Frame();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem,
+            this.车辆装货确认ToolStripMenuItem,
+            this.报表查询ToolStripMenuItem,
+            this.装车日志ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.系统设置ToolStripMenuItem,
+            this.车道管理ToolStripMenuItem,
+            this.pLC设置ToolStripMenuItem,
+            this.lED屏幕设置ToolStripMenuItem,
+            this.刷卡器设置ToolStripMenuItem});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // 系统设置ToolStripMenuItem
+            // 
+            this.系统设置ToolStripMenuItem.Name = "系统设置ToolStripMenuItem";
+            this.系统设置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.系统设置ToolStripMenuItem.Text = "平台设置";
+            // 
+            // 车道管理ToolStripMenuItem
+            // 
+            this.车道管理ToolStripMenuItem.Name = "车道管理ToolStripMenuItem";
+            this.车道管理ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.车道管理ToolStripMenuItem.Text = "车道管理";
+            // 
+            // pLC设置ToolStripMenuItem
+            // 
+            this.pLC设置ToolStripMenuItem.Name = "pLC设置ToolStripMenuItem";
+            this.pLC设置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pLC设置ToolStripMenuItem.Text = "PLC设置";
+            // 
+            // lED屏幕设置ToolStripMenuItem
+            // 
+            this.lED屏幕设置ToolStripMenuItem.Name = "lED屏幕设置ToolStripMenuItem";
+            this.lED屏幕设置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lED屏幕设置ToolStripMenuItem.Text = "LED屏幕设置";
+            // 
+            // 刷卡器设置ToolStripMenuItem
+            // 
+            this.刷卡器设置ToolStripMenuItem.Name = "刷卡器设置ToolStripMenuItem";
+            this.刷卡器设置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.刷卡器设置ToolStripMenuItem.Text = "刷卡器设置";
+            // 
+            // 车辆装货确认ToolStripMenuItem
+            // 
+            this.车辆装货确认ToolStripMenuItem.Name = "车辆装货确认ToolStripMenuItem";
+            this.车辆装货确认ToolStripMenuItem.Size = new System.Drawing.Size(92, 21);
+            this.车辆装货确认ToolStripMenuItem.Text = "车辆装货确认";
+            // 
+            // 报表查询ToolStripMenuItem
+            // 
+            this.报表查询ToolStripMenuItem.Name = "报表查询ToolStripMenuItem";
+            this.报表查询ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.报表查询ToolStripMenuItem.Text = "报表查询";
+            // 
+            // 装车日志ToolStripMenuItem
+            // 
+            this.装车日志ToolStripMenuItem.Name = "装车日志ToolStripMenuItem";
+            this.装车日志ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.装车日志ToolStripMenuItem.Text = "装车日志";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.Bage3Show, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Bage2Show, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Bage1Show, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 43);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.94118F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.05882F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1904, 996);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(3, 490);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(628, 503);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "车道一日志";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Location = new System.Drawing.Point(637, 490);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(628, 503);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "车道二日志";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Location = new System.Drawing.Point(1271, 490);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(628, 503);
+            this.groupBox6.TabIndex = 5;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "车道三日志";
+            // 
+            // Bage1Show
+            // 
+            this.Bage1Show.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Bage1Show.Location = new System.Drawing.Point(3, 3);
+            this.Bage1Show.Name = "Bage1Show";
+            this.Bage1Show.Size = new System.Drawing.Size(628, 481);
+            this.Bage1Show.TabIndex = 6;
+            this.Bage1Show.TabStop = false;
+            this.Bage1Show.Text = "车道一";
+            // 
+            // Bage2Show
+            // 
+            this.Bage2Show.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Bage2Show.Location = new System.Drawing.Point(637, 3);
+            this.Bage2Show.Name = "Bage2Show";
+            this.Bage2Show.Size = new System.Drawing.Size(628, 481);
+            this.Bage2Show.TabIndex = 7;
+            this.Bage2Show.TabStop = false;
+            this.Bage2Show.Text = "车道二";
+            // 
+            // Bage3Show
+            // 
+            this.Bage3Show.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Bage3Show.Location = new System.Drawing.Point(1271, 3);
+            this.Bage3Show.Name = "Bage3Show";
+            this.Bage3Show.Size = new System.Drawing.Size(628, 481);
+            this.Bage3Show.TabIndex = 8;
+            this.Bage3Show.TabStop = false;
+            this.Bage3Show.Text = "车道三";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "袋装装车控制系统";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 车道管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pLC设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lED屏幕设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刷卡器设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 车辆装货确认ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 报表查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 装车日志ToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private 控件.Frame Bage3Show;
+        private 控件.Frame Bage2Show;
+        private 控件.Frame Bage1Show;
     }
 }
 
---
+++ BageSystem/控件/Frame.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BageSystem.控件
+{
+   public class Frame: GroupBox
+    {
+
+        private string _text = "";
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public Frame()
+        {
+            base.Text = "";
+        }
+
+        [Browsable(true)]
+        [Category("Appearance")]
+        [DefaultValue("GroupBoxText")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public new string Text
+        {
+            get
+            {
+
+                return _text;
+            }
+            set
+            {
+
+                _text = value;
+                this.Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// 重绘
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //first let the base class to draw the control 
+            base.OnPaint(e);
+            //create a brush with fore color
+            SolidBrush colorBrush = new SolidBrush(this.ForeColor);
+            //create a brush with back color
+            var backColor = new SolidBrush(this.BackColor);
+            //measure the text size
+            var size = TextRenderer.MeasureText(this.Text, this.Font);
+            // evaluate the postiong of text from left;
+            int left = (this.Width - size.Width) / 2;
+            //draw a fill rectangle in order to remove the border
+            e.Graphics.FillRectangle(backColor, new Rectangle(left, 0, size.Width, size.Height));
+            //draw the text Now
+            e.Graphics.DrawString(this.Text, this.Font, colorBrush, new PointF(left, 0));
+        }
+
+
+
+
+    }
+}
